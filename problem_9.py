e4883b545de532ead31d572b167397e4d6efad85
aknemine <30924629+aknemine@users.noreply.github.com>
2017-08-15 16:11:05 +0300

    Update problem_9.py

--- problem_9.py
+++ problem_9.py
@@ -1,12 +1,12 @@
-def kare(x):
+def square(x):
     return x ** 2
 
 for i in range(5, 1000):
     for j in range(4, i):
         for k in range(3, j):
             if(i + j + k == 1000):
-                f = kare(i)
-                e = kare(j)
-                d = kare(k)
+                f = square(i)
+                e = square(j)
+                d = square(k)
                 if(e + d == f):
                     print(i * j * k)
